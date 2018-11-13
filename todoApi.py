a031cfb0e88e11adade81918541d300d7f35039a
amarteaux <alexandre.marteaux@outlook.fr>
2018-11-13 11:30:32 +0100

    added psycopg2 to requirement

--- todoApi.py
+++ todoApi.py
@@ -1,12 +1,7 @@
-# run a test server
 from api import create_app, db
-from config import DevelopmentConfig
 from api.models import Todo
 
-app = create_app(DevelopmentConfig)
-
-app.run(host='127.0.0.1', port=8080, debug=True)
-
+app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
